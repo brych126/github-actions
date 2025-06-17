6c46d11c853c4294de40e3bb41c7317d53933793
brych126 <brych.126@gmail.com>
2025-06-17 23:09:31 +0300

    Cache example

---
+++ RestSharpExample/Program.cs
@@ -0,0 +1,6 @@
+﻿using RestSharp;
+
+var client = new RestClient("https://httpbin.org");
+var request = new RestRequest("get", Method.Get);
+var response = client.Execute(request);
+Console.WriteLine(response.Content);
