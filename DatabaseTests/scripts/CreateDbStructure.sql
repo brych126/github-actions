6273cade03f228f7d617ca4357bafa0adf9d051f
brych126 <brych.126@gmail.com>
2025-08-02 16:24:05 +0300

    Breake db init script

--- DatabaseTests/scripts/CreateDbStructure.sql
+++ DatabaseTests/scripts/CreateDbStructure.sql
@@ -15,7 +15,7 @@ BEGIN
     DROP TABLE dbo.TestTable;
 END
 
-CREATE TABLE dbo.TestTable (
+CREAT TABLE dbo.TestTable (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Name NVARCHAR(100),
     CreatedAt DATETIME DEFAULT GETUTCDATE()
